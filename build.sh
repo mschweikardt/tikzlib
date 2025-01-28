859a7e6ae7fe81866aea38fadca9dfab2c365c9a
Matthias Schweikardt <mschweikardt@posteo.de>
2025-01-28 17:51:08 +0100

    add figs

--- build.sh
+++ build.sh
@@ -17,8 +17,9 @@ echo "  <body>" >> index.html
 echo "  <h2>tikzlib</h2>" >> index.html
 echo "  <p>Author:  <a href="https://github.com/mschweikardt">Matthias Schweikardt</a></p>" >> index.html
 echo "  <p>License: <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a></p>" >> index.html
-echo "  <p>This  <a href="$REPO">repository</a> contains various TikZ figures.</p>" >> index.html
-echo "  <b>Figures:</b>" >> index.html
+echo "  <p>Description: This  <a href="$REPO">repository</a> contains various <a href="https://ctan.org/pkg/pgf">TikZ</a> and <a href="https://ctan.org/pkg/circuitikz">CircuiTikZ</a> figures.</p>" >> index.html
+echo "  <p>Repository: <a href="$REPO">Link</a></p>" >> index.html
+echo "  <p>Figures:</p>" >> index.html
 echo "  <dl>" >> index.html
 
 cd test
