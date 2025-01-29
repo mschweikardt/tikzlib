cfe05c7f6fdd3531e8e122c05c64ff98b1755a61
Matthias Schweikardt <mschweikardt@posteo.de>
2025-01-29 04:48:59 +0100

    add compilation time

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ echo "  <p>Author:  <a href="https://github.com/mschweikardt">Matthias Schweikar
 echo "  <p>License: <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a></p>" >> index.html
 echo "  <p>Description: This  <a href="$REPO">repository</a> contains various <a href="https://ctan.org/pkg/pgf">TikZ</a> and <a href="https://ctan.org/pkg/circuitikz">CircuiTikZ</a> figures.</p>" >> index.html
 echo "  <p>Repository: <a href="$REPO">Link</a></p>" >> index.html
+echo "  <p>Compilation Date: `date +'%Y-%m-%d'`" >> index.html
 echo "  <p>Figures:</p>" >> index.html
 echo "  <dl>" >> index.html
 
